b0f349e29ecadd23a0a27dbf461d4ab98020da4b
Lorena Lobo <74051141+loaalobo@users.noreply.github.com>
2022-04-13 11:09:55 -0300

    refactor: altera os comentários do script.js e adiciona duas imagens

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    // card options
+    // cria um array com 12 objetos 'carta' (sendo 1 imagem para cada 2 cartas)
     const cardArray = [
         {
             name: 'puppy1',
@@ -52,17 +52,19 @@ document.addEventListener('DOMContentLoaded', () => {
         },
     ];
 
+    // ordena os objetos (12 cartas) dentro do array de forma aleatória
     cardArray.sort(() => 0.5 - Math.random());
 
+    // acessa os espaços que já criei no html tanto para o jogo em si quanto para a exibição do resultado
     const grid = document.querySelector('.grid');
     const resultDisplay = document.querySelector('#result');
-    let cardsChosen = [];
-    let cardsChosenId = [];
-    let cardsWon = [];
+    let cardsChosen = []; // cria uma lista para armazenar as duas cartas escolhidas por rodada
+    let cardsChosenId = []; // cria uma lista para armazenar os ids das duas cartas escolhidas por rodada
+    let cardsWon = []; // cria uma lista com as cartas que já deram match (combinadas corretamente)
 
-    // create your board
+    // cria o "tabuleiro" do jogo: para cada um dos 12 objetos no array de cartas, será criada uma imagem e setado um endereço para ela (img/img7.jpeg). Também será setado um atributo personalizado chamado 'id' com o valor de 0 a 11 (já que são 12 objetos no total). Por fim, adiciona um evento de click e chama a função flipCard para cada pbjeto. O 'grid.appendChild(card)' é responsável por adicionar a img criada ao espaço reservado no html, o qual acessamos através da variável 'const grid'
     function createBoard() {
-        for (i in cardArray) {
+        for (i in cardArray) { //length: 12
             const card = document.createElement('img');
             card.setAttribute('src', 'img/img7.jpeg');
             card.setAttribute('data-id', i);
@@ -71,7 +73,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    // check for matches
+    // checa as combinações
     function checkForMatch() {
         let cards = document.querySelectorAll('img');
         const optionOneId = cardsChosenId[0];
@@ -86,24 +88,25 @@ document.addEventListener('DOMContentLoaded', () => {
             cards[optionTwoId].setAttribute('src', 'img/img7.jpeg');
             alert('Sorry, try again');
         }
-        cardsChosen = [];
-        cardsChosenId = [];
-        resultDisplay.textContent = cardsWon.length;
-        if (cardsWon.length === cardArray.length/2) {
-            resultDisplay.textContent = 'Congratulations! You found them all!'
+        // a cada rodada, independentemente se acertou ou não a combinação, vai:
+        cardsChosen = []; // esvaziar a lista que armazenava as duas cartas escolhidas
+        cardsChosenId = []; // esvaziar a lista com os nº. de id das duas cartas escolhidas
+        resultDisplay.textContent = cardsWon.length; // atualiza o número de acertos
+        if (cardsWon.length === cardArray.length/2) { // quando alcançar 6 pontos vai:
+            resultDisplay.textContent = 'Congratulations! You found them all!' // exibir mensagem de 'parabéns'
         }
     }
 
-    // flip your card
+    // vira a carta
     function flipCard() {
-        let cardId = this.getAttribute('data-id');
-        cardsChosen.push(cardArray[cardId].name);
-        cardsChosenId.push(cardId);
-        this.setAttribute('src', cardArray[cardId].img)
-        if (cardsChosen.length === 2) {
-            setTimeout(checkForMatch, 500);
+        let cardId = this.getAttribute('data-id'); // acessa o id de cada um dos objetos do array de cartas
+        cardsChosen.push(cardArray[cardId].name); // salva o nome do objeto escolhido (carta clicada) na lista de escolhidas
+        cardsChosenId.push(cardId); // salva o id do objeto escolhido na lista de 'id das cartas escolhidas'
+        this.setAttribute('src', cardArray[cardId].img) // seta na carta que foi clicada, o endereço da imagem que estiver associada ao id 
+        if (cardsChosen.length === 2) { // quando a lista de cartas escolhidas contiver dois objetos vai: 
+            setTimeout(checkForMatch, 500); // chamar a função que checa a combinação
         }
     }
 
-    createBoard();
+    createBoard(); // chama a função que cria o tabuleiro do jogo (gostaria de criar um botão 'iniciar jogo')
 })
