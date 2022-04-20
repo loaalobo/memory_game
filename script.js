f807722bfbd07f0aab16f70da65bf68a3b36d4b6
Lorena Lobo <74051141+loaalobo@users.noreply.github.com>
2022-04-20 14:05:13 -0300

    refactor: add meta tag e comentários

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-    // cria um array com 12 objetos 'carta' (sendo 1 imagem para cada 2 cartas)
-    const cardArray = [
+document.addEventListener('DOMContentLoaded', () => { // o evento DOMContentLoaded é acionado quando todo o HTML foi completamente carregado e analisado, sem aguardar pelo CSS, imagens, e subframes para encerrar o carregamento. 
+    
+    // cria um array com 12 objetos 'carta' (1 imagem p/ cada 2 cartas)
+    const cardArray = [ 
         {
             name: 'puppy1',
             img: 'img/img1.jpeg'
@@ -58,11 +58,12 @@ document.addEventListener('DOMContentLoaded', () => {
     // acessa os espaços que já criei no html tanto para o jogo em si quanto para a exibição do resultado
     const grid = document.querySelector('.grid');
     const resultDisplay = document.querySelector('#result');
-    let cardsChosen = []; // cria uma lista para armazenar as duas cartas escolhidas por rodada
-    let cardsChosenId = []; // cria uma lista para armazenar os ids das duas cartas escolhidas por rodada
+
+    let cardsChosen = []; // cria uma lista para armazenar as 2 cartas escolhidas por rodada
+    let cardsChosenId = []; // cria uma lista para armazenar os ids das 2 cartas escolhidas por rodada
     let cardsWon = []; // cria uma lista com as cartas que já deram match (combinadas corretamente)
 
-    // cria o "tabuleiro" do jogo: para cada um dos 12 objetos no array de cartas, será criada uma imagem e setado um endereço para ela (img/img7.jpeg). Também será setado um atributo personalizado chamado 'id' com o valor de 0 a 11 (já que são 12 objetos no total). Por fim, adiciona um evento de click e chama a função flipCard para cada pbjeto. O 'grid.appendChild(card)' é responsável por adicionar a img criada ao espaço reservado no html, o qual acessamos através da variável 'const grid'
+    // cria o "tabuleiro" do jogo: para cada um dos 12 objetos no array de cartas, será criada uma imagem e setado um endereço para ela (img/img7.jpeg). Também será setado um atributo personalizado chamado 'id' com o valor de 0 a 11 (já que são 12 objetos no total). Por fim, adiciona um evento de click e chama a função flipCard para cada objeto. O 'grid.appendChild(card)' é responsável por adicionar a img criada ao espaço reservado no html, o qual acessamos através da variável 'const grid'
     function createBoard() {
         for (i in cardArray) { //length: 12
             const card = document.createElement('img');
@@ -102,7 +103,7 @@ document.addEventListener('DOMContentLoaded', () => {
         let cardId = this.getAttribute('data-id'); // acessa o id de cada um dos objetos do array de cartas
         cardsChosen.push(cardArray[cardId].name); // salva o nome do objeto escolhido (carta clicada) na lista de escolhidas
         cardsChosenId.push(cardId); // salva o id do objeto escolhido na lista de 'id das cartas escolhidas'
-        this.setAttribute('src', cardArray[cardId].img) // seta na carta que foi clicada, o endereço da imagem que estiver associada ao id 
+        this.setAttribute('src', cardArray[cardId].img) // seta, na carta que foi clicada, o endereço da imagem que estiver associada ao id 
         if (cardsChosen.length === 2) { // quando a lista de cartas escolhidas contiver dois objetos vai: 
             setTimeout(checkForMatch, 500); // chamar a função que checa a combinação
         }
